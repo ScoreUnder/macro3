13c3675561e27173ee44072ceea0f188f6cf6e21
Score_Under <seejay.11@gmail.com>
2021-07-11 19:07:44 +0100

    Minimize

--- src/main/scala/MacroVis.scala
+++ src/main/scala/MacroVis.scala
@@ -2,10 +2,8 @@ import scala.quoted._
 import java.util.IdentityHashMap
 
 class MacroVis[Q <: Quotes & Singleton](using val q: Q) {
-  import q.reflect._
-
-  given symbolInstance: Vis[q.reflect.Symbol] = new VisBuilder[Symbol]
-    .attributeReference("mayBeOwner")(_.maybeOwner)
+  given symbolInstance: Vis[q.reflect.Symbol] = new VisBuilder[q.reflect.Symbol]
+    .attributeReference[q.reflect.Symbol]("mayBeOwner")
     .build()
 }
 
@@ -17,7 +15,7 @@ type VisBuilderCtx = IdentityHashMap[AnyRef, Unit]
 case class VisBuilder[A](
   private val attrRefsFun: Vector[(String, String)] = Vector.empty,
 ):
-  def attributeReference[B: Vis](name: String)(f: A => B): VisBuilder[A] = copy(attrRefsFun = attrRefsFun :+ (name -> "name"))
+  def attributeReference[B: Vis](name: String): VisBuilder[A] = copy(attrRefsFun = attrRefsFun :+ (name -> "name"))
 
   def build(): Vis[A] = new Vis[A]:
     override def vis(a: A): Visualized =
